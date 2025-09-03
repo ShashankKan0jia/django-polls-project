7b95f0adefb00bec66293e2d0d67b5069edb8771
SHASHANK KANOJIA <115026120+ShashankKan0jia@users.noreply.github.com>
2025-09-03 18:11:40 +0530

    Add templates and fix generic views

--- polls/urls.py
+++ polls/urls.py
@@ -1,11 +1,13 @@
+# polls/urls.py
+
 from django.urls import path
 from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('owner', views.owner, name='owner'), # Path for the autograder
-    path('<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/results/', views.results, name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('owner', views.owner, name='owner'), # Stays the same
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'), # Changed to pk
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'), # Changed to pk
+    path('<int:question_id>/vote/', views.vote, name='vote'), # Stays the same
 ]
